591c70d654bb8bacb7e1d85ca5249c9309a15944
PedrooSilvaa <pedrohenriquefdasilva14@gmail.com>
2024-05-06 22:23:30 -0300

    change code

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import * as Page from "./src/class/styled-external"
 
+import {Text, View, StyleSheet, Platform} from 'react-native';
 
 export default function App() {
   return (
-    <Page.PageContainer>
-      <Page.PageTitle>
-        React Native
-      </Page.PageTitle>
-      <Page.PageSubTitle>
-        Styled-Components
-      </Page.PageSubTitle>
-    </Page.PageContainer>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+      <Text style={styles.paragraph}>World</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Platform.OS === "android" ? "blue" : "yellow"
+  },
+  title: {
+    color: "green",
+    fontSize: 100,
+    fontWeight: "bold"
+  },
+  paragraph: {
+    fontSize: 60,
+    color: "yellow",
+    fontWeight: "bold"
+  }
+})
---
+++ src/class/app-styles-external.js
@@ -0,0 +1,14 @@
+import * as Page from "./styled-external"
+
+export default function App() {
+  return (
+    <Page.PageContainer>
+      <Page.PageTitle>
+        React Native
+      </Page.PageTitle>
+      <Page.PageSubTitle>
+        Styled-Components
+      </Page.PageSubTitle>
+    </Page.PageContainer>
+  );
+}
